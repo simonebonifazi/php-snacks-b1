29319dd4671887fa1d3efa85ffd5b237e51dd768
simonebonifazi <simonbonifraz@gmail.com>
2022-08-30 17:50:29 +0200

    added-array-snack-1

---
+++ snack-1.php
@@ -0,0 +1,68 @@
+<!-- traccia first snack
+
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+-->
+<!-- BASKET
+ITALIA
+Lega A
+2022/2023
+
+
+Basket Napoli - Virtus Bologna
+Olimpia Milano-Brescia
+Tortona-Trento
+Treviso-Reggiana
+Trieste-Pesaro
+Varese-Sassari
+Venezia-Scafati
+Verona-Brindisi 
+-->
+<?php
+$basket_calendar = [
+    [
+        "home" => "Basket Napoli",
+        "away" => "Virtus Bologna",
+        "score" => "56 - 64",
+    ],
+    [
+        "home" => "Olimpia Milano",
+        "away" => "Brescia",
+        "score" => "82 - 81",
+    ],
+    [
+        "home" => "Tortona",
+        "away" => "Trento",
+        "score" => "67 - 74",
+    ],
+    [
+        "home" => "Trieste",
+        "away" => "Pesaro",
+        "score" => "55 - 45",
+    ],
+    [
+        "home" => "Treviso",
+        "away" => "Reggiana",
+        "score" => "68 - 84",
+    ],
+    [
+        "home" => "Varese",
+        "away" => "Sassari",
+        "score" => "77 - 52",
+    ],
+    [
+        "home" => "Venezia",
+        "away" => "Scafati",
+        "score" => "84 - 74",
+    ],
+    [
+        "home" => "Verona",
+        "away" => "Brindisi",
+        "score" => "66 - 54",
+    ],
+   
+    
+]
+?>
